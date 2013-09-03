766390e476c6d589177edfd6cc2fda586ec13643
François Boulogne <fboulogne sciunto org>
2013-09-03 21:30:50 +0200

    initialize

---
+++ README.md
@@ -0,0 +1,30 @@
+tear-coverage
+=============
+
+Simple script to remove the first page of a PDF
+
+Why?
+====
+
+Because many pdf providers such as scientific publishers like to add a useless cover page.
+I don't want to:
+* print the first page and sometimes I can forget to select the page range
+* to scroll down to read the file
+
+How to use?
+===========
+
+It's dead simple
+
+    tear-pages.py file.pdf
+
+
+License
+=======
+
+GPLv3. Read LICENSE for more details.
+
+Author
+======
+
+F. Boulogne.
---
+++ tear-pages.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Francois Boulogne
+# License: GPLv3
+
+import argparse
+import shutil
+import tempfile
+from PyPDF2 import PdfFileWriter, PdfFileReader
+
+
+def main(filename):
+    """
+    Copy filename to a tempfile, write pages 1..N to filename.
+
+    :param filename: PDF filepath
+    """
+    tmp = tempfile.NamedTemporaryFile()
+    shutil.copy(filename, tmp.name)
+
+    output_file = PdfFileWriter()
+    input_file = PdfFileReader(open(tmp.name, 'rb'))
+    num_pages = input_file.getNumPages()
+
+    for i in range(1, num_pages):
+        output_file.addPage(input_file.getPage(i))
+
+    tmp.close()
+    outputStream = open(filename, "wb")
+    output_file.write(outputStream)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Remove the first page of a PDF',
+                             epilog='')
+    #parser.add_argument('--version', action='version', version=info.NAME + ' ' + info.VERSION)
+    parser.add_argument('pdf', metavar='PDF', help='PDF filepath')
+    args = parser.parse_args()
+
+    main(args.pdf)

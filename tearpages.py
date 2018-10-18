f2828166ec1b93972d76b0173a55315eefb070ef
François Boulogne <devel@sciunto.org>
2018-10-18 12:00:02 +0200

    use with statements everywhere

--- tearpages.py
+++ tearpages.py
@@ -9,7 +9,7 @@ import tempfile
 from PyPDF2 import PdfFileWriter, PdfFileReader
 from PyPDF2.utils import PdfReadError
 
-__version__ = '0.2.2'
+__version__ = '0.3.0'
 
 
 def fixPdf(pdfFile, destination):
@@ -19,19 +19,17 @@ def fixPdf(pdfFile, destination):
     :param pdfFile: PDF filepath
     :param destination: destination
     """
-    tmp = tempfile.NamedTemporaryFile()
-    output = open(tmp.name, 'wb')
-    with open(pdfFile, "rb") as fh:
-        with open(pdfFile, "rb") as fh:
-            for line in fh:
-                output.write(line)
-                if b'%%EOF' in line:
-                    break
-    output.close()
-    shutil.copy(tmp.name, destination)
+    with tempfile.NamedTemporaryFile() as tmp:
+        with open(tmp.name, 'wb') as output:
+            with open(pdfFile, "rb") as fh:
+                for line in fh:
+                    output.write(line)
+                    if b'%%EOF' in line:
+                        break
+        shutil.copy(tmp.name, destination)
 
 
-def tearpage(filename, startpage=1):
+def tearpage(filename, startpage=0):
     """
     Copy filename to a tempfile, write pages startpage..N to filename.
 
@@ -39,26 +37,26 @@ def tearpage(filename, startpage=1):
     :param startpage: page number for the new first page
     """
     # Copy the pdf to a tmp file
-    tmp = tempfile.NamedTemporaryFile()
-    shutil.copy(filename, tmp.name)
+    with tempfile.NamedTemporaryFile() as tmp:
+        shutil.copy(filename, tmp.name)
 
-    # Read the copied pdf
-    try:
-        input_file = PdfFileReader(open(tmp.name, 'rb'))
-    except PdfReadError:
-        fixPdf(filename, tmp.name)
-        input_file = PdfFileReader(open(tmp.name, 'rb'))
-    # Seek for the number of pages
-    num_pages = input_file.getNumPages()
+        # Read the copied pdf
+        try:
+            input_file = PdfFileReader(open(tmp.name, 'rb'))
+        except PdfReadError:
+            fixPdf(filename, tmp.name)
+            input_file = PdfFileReader(open(tmp.name, 'rb'))
+        # Seek for the number of pages
+        num_pages = input_file.getNumPages()
 
-    # Write pages excepted the first one
-    output_file = PdfFileWriter()
-    for i in range(startpage, num_pages):
-        output_file.addPage(input_file.getPage(i))
+        # Write pages excepted the first one
+        output_file = PdfFileWriter()
+        for i in range(startpage, num_pages):
+            output_file.addPage(input_file.getPage(i))
 
-    tmp.close()
-    outputStream = open(filename, "wb")
-    output_file.write(outputStream)
+        
+    with open(filename, "wb") as outputStream:
+        output_file.write(outputStream)
 
 
 if __name__ == '__main__':
@@ -68,4 +66,4 @@ if __name__ == '__main__':
     parser.add_argument('pdf', metavar='PDF', help='PDF filepath')
     args = parser.parse_args()
 
-    tearpage(args.pdf)
+    tearpage(args.pdf, startpage=1)
